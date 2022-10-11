486b8cac10864e52e9231050c73289674c5c1b81
Peter Sieg <16632721+petersieg@users.noreply.github.com>
2022-10-11 20:20:35 +0200

    convert bin to hex
    Use jkcemu to load programs / games.
    save mem in bin format.
    Compile b2s and run it like: b2s mem.bin > mem.vhd
    Cut and paste mem.vhd into RAM.vhd..

---
+++ b2s.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/*************************************************************************************************/
+/* Conversion-functions */
+typedef unsigned char byte, *buffer;
+
+/*************************************************************************************************/
+int main(int argc, char *argv[])
+{
+    FILE* fp;
+    if(argc > 1)
+    {
+        buffer buff;
+        int idx = (-1);
+        unsigned int bytes_readed = 0;
+
+        if( (fp = fopen(argv[1], "rb")) == NULL )
+        {
+           puts("[!]Problem with opening the file.");
+           exit;
+        }
+        else
+           puts("File has been opened successfully.\n");
+
+        /* Finds the file size, allocates memory for the buffer and copies the file within the buffer (faster processing) */
+        unsigned int fp_size;
+        fseek(fp, 0, SEEK_END);
+        fp_size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        buff = calloc(fp_size, sizeof(byte));
+        fread(buff, sizeof(byte), fp_size, fp);
+        fclose(fp);
+        
+        while( ++idx <= ( fp_size - 1) )
+        {
+            byte current = buff[idx];
+
+            //printf("%05u;'%04X;'%02X\n", bytes_readed, bytes_readed, current);
+	    printf(" 16#%02X#,", current);
+            if ((idx % 10) == 0) printf("\n");
+            bytes_readed++;
+        }
+        free(buff);
+    }
+
+}
+
